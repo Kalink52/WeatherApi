841fde3249e658e3770602fc6ff0c97d6fea9740
Kalink52 <micah.cox52@gmail.com>
2024-05-18 17:19:40 -0400

    got current day weather information to display using Jquery

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,6 +2,8 @@
 console.log('hello world');
 const apiKey = '113f4e0e1aa399ec7fa92159348d450c'
 
+// let activeCityRef = document.getElementById("activeCity") 
+
 // temp lat and long 
 // let lat = 28.5783
 // let lon = 81.8867
@@ -15,11 +17,11 @@ function getLocation(city) {
     })
     .then(function (data){
         // console.log(data)
-        testWeather(data);
+        getWeather(data);
     } )
 }
 
-function testWeather(geodata) {
+function getWeather(geodata) {
     console.log(geodata)
     console.log(geodata[0].lat)
     let lat = geodata[0].lat
@@ -31,9 +33,31 @@ function testWeather(geodata) {
     })
     .then(function (data){
         console.log(data)
-        
+        activeCity(data)
+        fivedayforcast(data)
+
     } )
 }
+function activeCity(city){ 
+    console.log(city)
+    //K to F converter
+    let tempInF = ((Number(city.list[0].main.temp) - 273.15) * 9/5 + 32).toFixed(0)
+    
+    const cityObject =
+    $(`<div class="fw-bold p-2"> ${city.city.name} ${city.list[0].dt}</div>
+        <div class="fw-bold p-2">temp: ${tempInF}° K</div>
+        <div class="fw-bold p-2">Wind: ${city.list[0].wind.speed}MPH</div>
+        <div class="fw-bold p-2">Humidity: ${city.list[0].main.humidity}%</div>
+        `)
+$('#activeCity').append(cityObject)
+return
+}
+
+function fivedayforcast (daysObject){
+    console.log(`daysObject`)
+
+}
+
 
 // update this to userinput
 getLocation(city)
